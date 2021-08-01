7da5c77d31c7a7af2caa70b790f4892c82d3f53b
kslee <kslee@ksleeui-Macmini.local>
2021-08-02 00:22:41 +0900

    add a App class to toggle isLoading.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  state = {
+    isLoading : true
+  };
+
+  componentDidMount(){
+    console.log("didMount");
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
+  }
+
+  render(){
+    console.log("render");
+    const {isLoading} = this.state;
+    return (<div> 
+        <h1>{isLoading ? "Loading" : "we are ready"}</h1>
+       </div>)
+  }
 }
 
 export default App;
